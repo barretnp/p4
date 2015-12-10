71985781621eddfd5b0e5dec6014661fe35ea6ea
Easterbrooks Paul <easterpa@l14349.jmu.edu>
2015-12-10 02:15:05 -0500

    added shell scripts

---
+++ killall.sh
@@ -0,0 +1,8 @@
+HOSTS="l25008 l25009 l25010 l25011 l25012"
+USERNAME=$1
+SCRIPT="killall -9 test"
+for HOSTNAME in ${HOSTS} ; do
+        #gnome-terminal -e "ssh -X $1@${CLIENTS[i]}" &       
+        echo "${HOSTNAME}:"
+	ssh -X $1@${HOSTNAME} "${SCRIPT}"
+done
---
+++ launch_clients.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SERV_IP=0x867E8DDE
+
+HOSTS="l25008 l25009 l25010 l25011 l25012"
+USERNAME=$1
+SCRIPT="cd ~/FA15/CS361/phase4; ./test"
+
+if [ -n "$USERNAME" ]
+then
+    for HOSTNAME in ${HOSTS} ; do
+	echo "Launching client on ${HOSTNAME}.."
+	gnome-terminal -e "ssh ${USERNAME}@${HOSTNAME} ${SCRIPT}"	
+	#ssh -f ${USERNAME}@${HOSTNAME} "${SCRIPT}"
+    done
+else 
+    echo "username required"
+fi
+
